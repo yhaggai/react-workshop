36ee4a033c8e4ed2a6cdee21c1c18759932f0b16
haggai <yhaggai@gmail.com>
2016-08-27 02:19:26 +0300

    first compoenent

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -1,4 +1,16 @@
 /// <reference path="../../typings/react/react.d.ts"/>
-import '../css/style.styl';
+/// <reference path="../../typings/react-dom/react-dom.d.ts"/>
 
+import '../css/style.styl';
+import * as ReactDOM from 'react-dom';
 import * as React from 'react';
+
+const StorePicker = React.createClass({
+  render: function() {
+    return (
+      <p>hi</p>
+    );
+  }
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
